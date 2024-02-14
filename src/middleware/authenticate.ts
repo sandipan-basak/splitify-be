8874c8ca9fe0030563f07a184ff8b8f8c3e7bba2
Sandy <sandy@Aashays-MacBook-Pro.local>
2024-02-15 03:32:27 +0530

    fix auth middleware to get the correct sid

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -4,10 +4,16 @@ import jwt from 'jsonwebtoken';
 const secretKey = process.env.SECRET_KEY!;
 
 const authenticate = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
+  const cookieHeader = req.headers.cookie;
+  const cookies: { [key: string]: string } = {};
 
-  if (token == null) return res.sendStatus(401);
+  cookieHeader?.split(';').forEach(cookie => {
+    const [name, value] = cookie.split('=').map(c => c.trim());
+    cookies[name] = value;
+  });
+  const token = cookies.sid;
+
+  if (!token) return res.sendStatus(401);
 
   jwt.verify(token, secretKey, (err, user) => {
     if (err) return res.sendStatus(403);
