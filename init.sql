56765c8d39a6435c4663f66bfc96e8866244c50c
Sandy <sandy@Aashays-MacBook-Pro.local>
2024-02-14 23:16:37 +0530

    add other table creation commands in the init sql file

--- init.sql
+++ init.sql
@@ -2,9 +2,35 @@
 \c database1;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE,
     phone_number VARCHAR(20) UNIQUE,
     password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE groups (
+    group_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE expenses (
+    expense_id SERIAL PRIMARY KEY,
+    group_id INTEGER REFERENCES groups(group_id),
+    amount DECIMAL(10, 2) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_by_user_id INTEGER REFERENCES users(user_id)
+);
+
+CREATE TABLE payments (
+    payment_id SERIAL PRIMARY KEY,
+    expense_id INTEGER NOT NULL REFERENCES expenses(expense_id),
+    user_id INTEGER NOT NULL REFERENCES users(user_id),
+    amount DECIMAL(10, 2) NOT NULL,
+    is_payee BOOLEAN NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    currency VARCHAR(3) NOT NULL DEFAULT 'INR'
 );
